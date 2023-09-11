117c7b8bd8e1647d2ef78057546f11780e55091d
Citrus <savelijpetrov38@gmail.com>
2023-09-11 20:52:09 +0300

    AddErrorMiddleware

---
+++ ProductService.Presentation/Middlewares/ErrorMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using FluentValidation;
+
+namespace ProductService.Presentation.Middlewares;
+
+public class ErrorMiddleware
+{
+    private readonly RequestDelegate _next;
+    
+    public ErrorMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            ExceptionHandler(context, e);
+        }
+    }
+
+    private static Task ExceptionHandler(HttpContext context, Exception e)
+    {
+        var statusCode = StatusCodes.Status500InternalServerError;
+        var result = "Error";
+        switch (e)
+        {
+            case ValidationException exception:
+                statusCode = StatusCodes.Status400BadRequest;
+                result = JsonSerializer.Serialize(exception.Errors);
+                break;
+        }
+
+        context.Response.StatusCode = statusCode;
+        context.Response.ContentType = "application/json";
+
+        return context.Response.WriteAsync(result);
+    }
+}
--- ProductService.Presentation/Program.cs
+++ ProductService.Presentation/Program.cs
@@ -1,5 +1,6 @@
 using ProductService.Application;
 using ProductService.Persistance;
+using ProductService.Presentation.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ErrorMiddleware>();
 
 app.UseHttpsRedirection();
 
